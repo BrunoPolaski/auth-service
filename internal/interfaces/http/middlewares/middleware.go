ce33e488ced26b1f5f8890c1b231ceb72c862e5c
BrunoPolaski <polaskibruno03@gmail.com>
2025-03-13 15:11:31 -0300

    feat: removing the stack function at the middleware

--- internal/interfaces/http/middlewares/middleware.go
+++ internal/interfaces/http/middlewares/middleware.go
@@ -3,10 +3,3 @@ package middlewares
 import "net/http"
 
 type Middleware func(http.Handler) http.Handler
-
-func CreateStack(middlewares []Middleware, finalHandler http.Handler) http.Handler {
-	for i := len(middlewares) - 1; i >= 0; i-- {
-		finalHandler = middlewares[i](finalHandler)
-	}
-	return finalHandler
-}
