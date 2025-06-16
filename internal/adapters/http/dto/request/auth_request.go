4be4532c2cec667f50d16682e9e6bd63901ea645
BrunoPolaski <polaskibruno03@gmail.com>
2025-06-15 21:39:00 -0300

    refactor: removing lambda

--- internal/adapters/http/dto/request/auth_request.go
+++ internal/adapters/http/dto/request/auth_request.go
@@ -1,4 +1,4 @@
-package dto
+package request
 
 type AuthRequest struct {
 	Username string `json:"username" validate:"required,email"`
--- main.go
+++ main.go
@@ -3,19 +3,19 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/BrunoPolaski/auth-service/internal/adapters/http/routes"
 	"github.com/BrunoPolaski/auth-service/internal/config/logger"
-	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/awslabs/aws-lambda-go-api-proxy/httpadapter"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatalf("Error loading .env file: %s", err)
+		err = godotenv.Load("../.env")
+		if err != nil {
+			log.Fatalf("%s", err)
+		}
 	}
 	logger.InitLogger()
 	logger.Info("Starting application")
@@ -26,9 +26,5 @@ func main() {
 		return
 	}
 
-	if os.Getenv("AWS_LAMBDA_FUNCTION_NAME") == "" {
-		log.Fatal(http.ListenAndServe(":8080", r))
-	} else {
-		lambda.Start(httpadapter.New(r).ProxyWithContext)
-	}
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
